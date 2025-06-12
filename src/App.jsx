00e60bfaac08c95098d7b273c16f385d43141b38
Dony <doniananda622@gmail.com>
2025-06-12 13:48:31 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  const [isStandalone, setIsStandalone] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.matchMedia) {
+      const isStandaloneMode = window.matchMedia('(display-mode: standalone)').matches;
+      console.log('Standalone mode:', isStandaloneMode);
+      setIsStandalone(isStandaloneMode);
+    }
+  }, []);
+
+  return (
+      <div style={{
+        maxWidth: 400,
+        margin: "100px auto",
+        padding: 24,
+        borderRadius: 12,
+        boxShadow: "0 2px 16px rgba(0,0,0,0.12)",
+        textAlign: "center"
+      }}>
+        <h1>React Vite PWA</h1>
+        <p>Aplikasi simple dengan Vite + React + PWA</p>
+        <div>
+          <b>Status PWA:</b>{" "}
+          {isStandalone ? "Sudah diinstall (Standalone)" : "Belum diinstall"}
+        </div>
+      </div>
+  );
+}
+
+
+
+export default App;
