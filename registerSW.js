1ba66ab4ed0971e8487c5fac025deb1f1d27e1d5
Dony <doniananda622@gmail.com>
2025-06-12 17:40:55 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/reactJs/sw.js', { scope: '/reactJs/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
