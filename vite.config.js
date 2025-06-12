7d13c09bddc50172e9dc7d5b953970b7b8fdd842
Dony <doniananda622@gmail.com>
2025-06-12 17:56:10 +0700

    tambahan banyak

--- vite.config.js
+++ vite.config.js
@@ -7,22 +7,27 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        short_name: 'ESTA',
-        name: 'Employment Start Tracking App',
-        start_url: '/reactJs/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#1976d2',
-        icons: [
-          {
-            src: 'assets/profile/icon.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+  registerType: 'autoUpdate',
+  manifest: {
+    short_name: 'ESTA',
+    name: 'Employment Start Tracking App',
+    start_url: '/reactJs/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#1976d2',
+    icons: [
+      {
+        src: 'assets/profile/icon.png', // pastikan path dan file benar
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'assets/profile/icon.png', // direkomendasikan juga punya 192x192
+        sizes: '192x192',
+        type: 'image/png'
       }
-    })
+    ]
+  }
+})
   ]
 })
